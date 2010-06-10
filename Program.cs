6b2ba83df2c0208636639aa1bec09c54033bc161
garretts <garretts@microsoft.com>
2010-06-10 10:23:36 -0700

    Created Project Skeleton

---
+++ Program.cs
@@ -0,0 +1,102 @@
+﻿//-----------------------------------------------------------------------
+// <copyright company="Codeplex Foundation">
+//     Copyright (c) 2010 Garrett Serack. All rights reserved.
+// </copyright>
+//-----------------------------------------------------------------------
+
+namespace CoApp.Scan {
+    using System;
+    using Toolkit.Extensions;
+
+    internal class Program {
+        private const string help = @"
+Usage:
+-------
+
+CoApp-scan [options] <source-root-path>
+
+    <source-root-path>          the root of the source tree to collect data from
+        
+    Options:
+    --------
+    --help                      this help
+    --nologo                    don't display the logo
+    --load-config=<file>        loads configuration from <file>
+    --verbose                   prints verbose messages
+    
+    --output-file=<file>        dumps the scan output to the specified <file>
+";
+
+        private static int Main(string[] args) {
+            return new Program().main(args);
+        }
+
+        private int main(string[] args) {
+            
+            var options = args.Switches();
+            var parameters = args.Parameters();
+
+            #region Parse Options 
+
+            foreach(var arg in options.Keys) {
+                var argumentParameters = options[arg];
+
+                switch(arg) {
+                        /* options  */
+                        
+                    case "output-file":
+                        // 
+                        break;
+                    
+                        /* global switches */
+                    case "load-config":
+                        // all ready done, but don't get too picky.
+                        break;
+
+                    case "nologo":
+                        this.Assembly().SetLogo("");
+                        break;
+
+                    case "help":
+                        return Help();
+
+                    default:
+                        return Fail("Unknown parameter [--{0}]", arg);
+                }
+            }
+            Logo();
+
+            #endregion
+
+            if(parameters.Count != 1) {
+                return Fail("Missign source code root path. \r\n\r\n    Use --help for command line help.");
+            }
+
+            return 0;
+        }
+
+        #region fail/help/logo
+
+        public int Fail(string text, params object[] par) {
+            Logo();
+            using(new ConsoleColors(ConsoleColor.Red, ConsoleColor.Black))
+                Console.WriteLine("Error:{0}", text.format(par));
+            return 1;
+        }
+
+        private int Help() {
+            Logo();
+            using(new ConsoleColors(ConsoleColor.White, ConsoleColor.Black))
+                help.Print();
+            return 0;
+        }
+
+        private void Logo() {
+            using(new ConsoleColors(ConsoleColor.Cyan, ConsoleColor.Black))
+                this.Assembly().Logo().Print();
+            this.Assembly().SetLogo("");
+        }
+
+        #endregion
+    }
+}
---
+++ Properties/AssemblyInfo.cs
@@ -0,0 +1,42 @@
+﻿//-----------------------------------------------------------------------
+// <copyright company="Codeplex Foundation">
+//     Copyright (c) 2010 Garrett Serack . All rights reserved.
+// </copyright>
+//-----------------------------------------------------------------------
+
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following 
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("CoApp.CLI")]
+[assembly: AssemblyDescription("CoApp command line utility")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("Codeplex Foundation")]
+[assembly: AssemblyProduct("CoApp Command Line Interface")]
+[assembly: AssemblyCopyright("Copyright © Garrett Serack, CoApp Contributors 2010")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible 
+// to COM components.  If you need to access a type in this assembly from 
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("12726103-fd13-46c3-bce5-48540532142e")]
+
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+//      Minor Version 
+//      Build Number
+//      Revision
+//
+// You can specify all the values or you can default the Build and Revision Numbers 
+// by using the '*' as shown below:
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
