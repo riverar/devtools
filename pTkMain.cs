5aab4323c2bed33c909cfc095ff933c22df6e212
Garrett Serack <garretts@microsoft.com>
2011-07-28 13:41:19 -0700

    Updated source files to include license information

--- pTkMain.cs
+++ pTkMain.cs
@@ -2,6 +2,10 @@
 // <copyright company="CoApp Project">
 //     Copyright (c) 2011 Garrett Serack. All rights reserved.
 // </copyright>
+// <license>
+//     The software is licensed under the Apache 2.0 License (the "License")
+//     You may not use the software except in compliance with the License. 
+// </license>
 //-----------------------------------------------------------------------
 
 namespace CoApp.Ptk {
