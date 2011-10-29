c50fee3cc52ae897a37deb5fedd45382d566df5f
Garrett Serack <garretts@microsoft.com>
2011-10-28 17:25:26 -0700

    Whoops. Forgot the new file; doesn't do anything yet

---
+++ PortableExecutable/PEAnalyzer.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace CoApp.Developer.Toolkit.PortableExecutable {
+    using CoApp.Toolkit.Exceptions;
+    using CoApp.Toolkit.Extensions;
+    using Microsoft.Cci;
+
+    public class PEAnalyzer {
+
+        /// <summary>
+        /// Not Even Started.
+        /// </summary>
+        /// <param name="filename"></param>
+        public static void Load(string filename) {
+             MetadataReaderHost _host = new PeReader.DefaultHost();
+
+             var module = _host.LoadUnitFrom(filename) as IModule;
+
+             if (module == null || module is Dummy) {
+                 throw new CoAppException("{0} is not a PE file containing a CLR module or assembly.".format(filename));
+             }
+             var ILOnly = module.ILOnly;
+
+            Console.WriteLine("module: {0}", module);
+        }
+    }
+}
