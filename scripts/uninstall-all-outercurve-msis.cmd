70b94e79d1841eaa8a4f003096d1c7a10b602d82
Garrett Serack <garretts@microsoft.com>
2011-12-28 18:54:03 -0800

    removed useless scripts

--- scripts/uninstall-all-outercurve-msis.cmd
+++
@@ -1,6 +0,0 @@
-REM wmic product get name 
-c:\apps\bin\coapp.service.exe --uninstall
-sc delete "CoApp Package Installer Service"
-
-wmic product where vendor='OUTERCURVE FOUNDATION' call uninstall
-rmdir /s /q c:\apps
