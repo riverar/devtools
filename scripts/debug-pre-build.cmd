a6905c9f5d039ed7fcee7e6975c5eed3ee7b2a60
Garrett Serack <garretts@microsoft.com>
2011-12-28 18:15:20 -0800

    fixn build stuff

--- scripts/debug-pre-build.cmd
+++ scripts/debug-pre-build.cmd
@@ -5,5 +5,5 @@ if exist "%~dp0\..\..\coapp\output\any\debug\bin\CoApp.Toolkit.dll" xcopy  /Q /D
 if exist "%~dp0\..\..\coapp\output\any\debug\bin\CoApp.Toolkit.Debug.dll" xcopy /Q /D /Y "%~dp0\..\..\coapp\output\any\debug\bin\CoApp.Toolkit.Debug.dll" "%~dp0\..\binaries"
 if exist "%~dp0\..\..\coapp\output\any\debug\bin\CoApp.Toolkit.Engine.Client.dll" xcopy /Q /D /Y "%~dp0\..\..\coapp\output\any\debug\bin\CoApp.Toolkit.Engine.Client.dll" "%~dp0\..\binaries"
 
-xcopy /Q /D /Y "%~dp0\..\binaries\*.dll" "%~dp0\..\output\any\debug\bin"
-xcopy /Q /D /Y "%~dp0\..\binaries\*.exe" "%~dp0\..\output\any\debug\bin"
+xcopy /Q /D /Y "%~dp0\..\ext\binaries\*.dll" "%~dp0\..\output\any\debug\bin"
+xcopy /Q /D /Y "%~dp0\..\ext\binaries\*.exe" "%~dp0\..\output\any\debug\bin"
--- scripts/release-pre-build.cmd
+++ scripts/release-pre-build.cmd
@@ -1,10 +1,18 @@
 @echo off
 setlocal
 
-cd "%~dp0\..\binaries"
-echo Y | erase *.*
+cd "%~dp0\..\ext\binaries" || goto failed
+echo Y | erase *.* || goto failed
 
-git reset --hard HEAD
+git reset --hard HEAD || goto failed
 
-xcopy /Q /D /Y "%~dp0\..\binaries\*.dll" "%~dp0\..\output\any\release\bin"
-xcopy /Q /D /Y "%~dp0\..\binaries\*.exe" "%~dp0\..\output\any\release\bin"
+xcopy /Q /D /Y "%~dp0\..\ext\binaries\*.dll" "%~dp0\..\output\any\release\bin" || goto failed
+xcopy /Q /D /Y "%~dp0\..\ext\binaries\*.exe" "%~dp0\..\output\any\release\bin" || goto failed
+
+REM Everything went ok!
+exit /b 0
+
+REM Something not ok :(
+:failed
+echo ERROR: Failure in script. aborting.
+exit /b 1
